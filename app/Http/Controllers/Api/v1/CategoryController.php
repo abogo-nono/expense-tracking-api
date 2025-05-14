a272b78513951f11123af17e991c6fe4a4ad0354
ABOGO <abogonono1@gmail.com>
2025-05-14 22:55:36 +0100

    feat: Expense category's controller created. An endpoint created to get all categories from db and return a collection (with pagination if needed)

---
+++ app/Http/Controllers/Api/v1/CategoryController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\CategoryResource;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $paginate = $request->get('paginate');
+        $categories = $paginate
+            ? Category::orderBy('name')->paginate($paginate)
+            : Category::orderBy('name')->get();
+
+        return CategoryResource::collection($categories);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Category $category)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Category $category)
+    {
+        //
+    }
+}
