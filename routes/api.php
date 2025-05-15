2927ef5ae4c5ca0f7dc207fe0c840c37d133e70b
ABOGO <abogonono1@gmail.com>
2025-05-15 21:13:00 +0100

    feat: prefix added to routes for versioning

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\Api\v1\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::prefix('v1')->group(function () {
+    Route::apiResource('categories', CategoryController::class);
+});
+
