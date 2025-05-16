5035b67d06b49b0b9464df4b5396e5ca6a190118
ABOGO <abogonono1@gmail.com>
2025-05-17 00:46:41 +0100

    feat: add budgets API resource route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\v1\BudgetController;
 use App\Http\Controllers\Api\v1\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -10,5 +11,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
     Route::apiResource('categories', CategoryController::class);
+    Route::apiResource('budgets', BudgetController::class);
 });
 
